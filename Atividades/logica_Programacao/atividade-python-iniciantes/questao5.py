fe64a36b55f05d31ede2e3e596ac859e790fa6ff
Antonio Barros <antonio.barros.alcantara.neto@ccc.ufcg.edu.br>
2025-08-22 22:13:34 -0300

    direc(atividade-python-iniciantes): Implementação do Extra da questäo 5

--- Atividades/logica_Programacao/atividade-python-iniciantes/questao5.py
+++ Atividades/logica_Programacao/atividade-python-iniciantes/questao5.py
@@ -1,10 +1,19 @@
 # Gerar acrônimo
 
 frase = input("Digite uma frase: ")
+stopwords_input = input("Digite as stopwords separadas por vírgula (ou Enter para padrão): ")
+
+stopwords_padrao = ["de", "da", "do", "das", "dos", "e"]
+
+if stopwords_input:
+    stopwords = [palavra.strip() for palavra in stopwords_input.split(",")]
+else:
+    stopwords = stopwords_padrao
+
 palavras = frase.split()
 acronimo = ""
 for palavra in palavras:
-    if palavra not in ["de", "da", "do", "das", "dos", "e"]:
+    if palavra not in stopwords:
         acronimo += palavra[0].upper()
         
 print(f"{acronimo}")
