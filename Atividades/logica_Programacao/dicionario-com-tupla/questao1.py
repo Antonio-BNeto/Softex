648565f0b1dbd17cdcb4db9f6db4b875193faba7
Antonio Barros <128563411+Antonio-BNeto@users.noreply.github.com>
2025-08-29 18:38:28 +0000

    feat(dicionario-com-tupla): Implementação da resolução da questão 1

---
+++ Atividades/logica_Programacao/dicionario-com-tupla/questao1.py
@@ -0,0 +1,9 @@
+pessoa = {
+    "nome": "Carla",
+    "idade": 19,
+    "cidade": "João Pessoa"
+}
+
+print("Nome: ",pessoa["nome"])
+print("Idade: ", pessoa["idade"])
+print("Cidade: ", pessoa["cidade"])
