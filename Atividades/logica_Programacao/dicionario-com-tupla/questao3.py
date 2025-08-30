f81d8b27e74747366e655857052171143ad33628
Antonio Barros <antonio.barros.alcantara.neto@ccc.ufcg.edu.br>
2025-08-30 00:09:01 -0300

    fix(dicionario-com-tuplas): Adicionando a resolução da questão 3

--- Atividades/logica_Programacao/dicionario-com-tupla/questao3.py
+++ Atividades/logica_Programacao/dicionario-com-tupla/questao3.py
@@ -1,5 +1,9 @@
 cidades = {
-	() : "São Paulo",
-	() : "Recife",
+	(-23.5505, -46.6333) : "São Paulo",
+	(-8.0476, -34.8770) : "Recife",
     (-7.2210, -35.8731): "Campina Grande - PB"
 }
+
+print(cidades[(-23.5505, -46.6333)])
+print(cidades[(-8.0476, -34.8770)])
+print(cidades[(-7.2210, -35.8731)])
